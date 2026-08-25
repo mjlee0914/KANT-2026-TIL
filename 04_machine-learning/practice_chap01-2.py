c1e8e763b31e7599bf000cc314fe4c9bcbee0457
mjlee0914 <myungjilee0914@gmail.com>
2026-08-25 17:52:32 +0900

    260825 TIL

--- 04_machine-learning/practice_chap01-2.py
+++ 04_machine-learning/practice_chap01-2.py
@@ -1,35 +1,17 @@
+# 1장 2강 문제 1
 import pandas as pd
 
-df = pd.read_csv('C:/Users/myung/Documents/KANT-2026-TIL/04_machine-learning/dataset.csv')
+df = pd.read_csv("dataset.csv")
 
-#------------------------------------------------1
-print("=" * 100)
-print(df.info())
-print("=" * 100)
-print(df.isnull().sum()) #결측치 파악
-print("=" * 100)
+print("중복된 행 개수:", df.duplicated().sum())
 
-mode_artists = df['artists'].mode()[0]
-print(mode_artists) # 아티스트 최빈값
+df = df.drop_duplicates().reset_index(drop=True)
 
-df['artists'] = df['artists'].fillna(mode_artists)
+print("=" * 50)
+print("수정 전:")
+print(df['duration_ms'].describe())
 
-
-mode_track_name = df.loc[:, 'track_name'].mode()[0] 
-print(mode_track_name) # 노래제목 최빈값
-
-df['track_name'] = df.loc[:,'track_name'].fillna(mode_track_name)
-
-print("=" * 100)
-print(df.describe())
-
-print("=" * 100)
-print(df['tempo'])
-print("=" * 100)
-print(df['popularity'])
-print("=" * 100)
-#-------------------------------------------------------2
-
-print(df.duplicated().sum())
-
-
+df = df.query('duration_ms >= 0 or duration_ms <= 1e6')
+print("=" * 50)
+print("수정 후:")
+print(df['duration_ms'].describe())
